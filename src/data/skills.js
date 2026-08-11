847c377014e5ade297ad8644429e4e0f6127c41c
Suseendhar T <tsuseendhar@gmail.com>
2026-08-11 17:03:45 +0530

    Updated portfolio

--- src/data/skills.js
+++ src/data/skills.js
@@ -7,7 +7,7 @@ const skills = [
   { name: "REST APIs", status: "done" },
   { name: "MongoDB", status: "done" },
   { name: "Git", status: "done" },
-  { name: "Node.js / Express", status: "done" },
+  { name: "Node.js / Express", status: "Done" },
 ];
 
 export default skills;

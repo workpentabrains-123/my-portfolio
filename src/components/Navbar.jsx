7c021830ce50eba03d305fb730594cda8cfd4b67
Suseendhar T <tsuseendhar@gmail.com>
2026-08-13 13:54:33 +0530

    Remove repo chips from project cards

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,45 @@
+// import logo from "../assets/logo.png";
+// function Navbar() {
+//   return (
+//     <nav className="navbar">
+//       <div className="logo">
+//   <img src={logo} alt="Susee Logo" className="logo-img" />
+//   <span>
+//     susee<span>.dev</span>
+//   </span>
+// </div>
+      
+
+//       <ul>
+//         <li>
+//           <a href="#about">About</a>
+//         </li>
+//         <li>
+//           <a href="#skills">Skills</a>
+//         </li>
+//         <li>
+//           <a href="#projects">Projects</a>
+//         </li>
+//         <li>
+//           <a href="#contact">Contact</a>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
 import logo from "../assets/logo.png";
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-  <img src={logo} alt="Susee Logo" className="logo-img" />
-  <span>
-    susee<span>.dev</span>
-  </span>
-</div>
-      
+        <img src={logo} alt="Susee Logo" className="logo-img" />
+        <span>
+          susee<span>.dev</span>
+        </span>
+      </div>
 
       <ul>
         <li>
@@ -22,6 +53,11 @@ function Navbar() {
         </li>
         <li>
           <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="/Suseendhar_Resume.pdf" target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
         </li>
       </ul>
     </nav>
